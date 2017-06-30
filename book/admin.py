d33074631841d44bf223feda3399e27c75c293f9
udit01 <uditjainiitd@gmail.com>
2017-07-01 00:31:28 +0530

    registered models correctly with admin

--- book/admin.py
+++ book/admin.py
@@ -3,7 +3,14 @@ from .models  import QRcode
 from oAuth.models import UserProfile,User
 # Register your models here.
 
-admin.site.register(QRcode)
-admin.site.register(User)
-admin.site.register(UserProfile)
+class CustomAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
+        super(CustomAdmin, self).__init__(model, admin_site)
+
+
+
+admin.site.register(QRcode,CustomAdmin)
+admin.site.register(User,CustomAdmin)
+admin.site.register(UserProfile,CustomAdmin)
 
